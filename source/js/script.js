66d73da102fc4359d2591e33d611e3369fc9f59f
evsenia <evsenia@mail.ru>
2023-08-23 22:22:30 +0700

    index

---
+++ source/js/script.js
@@ -0,0 +1,16 @@
+const sliderButton = document.querySelector('.slider__button');
+
+const slider = document.querySelector('.slider');
+
+if(sliderButton){
+  sliderButton.addEventListener('click', () => {
+    const stateBefore = document.querySelector('.slider--state-before');
+    const stateAfter = document.querySelector('.slider--state-after');
+    if (stateBefore || stateAfter) {
+      slider.classList.toggle('slider--state-before');
+      slider.classList.toggle('slider--state-after');
+    } else {
+      slider.classList.add('slider--state-after');
+    }
+  });
+}
